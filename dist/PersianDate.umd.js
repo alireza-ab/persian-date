76628650fc1ccaddd725032b060dac0c4bd5f119
Alireza Alibeiki <alibeikialireza@gmail.com>
2022-08-03 10:33:27 +0430

    build and release

--- dist/PersianDate.umd.js
+++ dist/PersianDate.umd.js
@@ -416,17 +416,22 @@
 	  };
 	}
 
-	function isBetween(from, to, method) {
-	  var date = this.toArray();
-	  from = typesToArray(this.c, from); // if (!from[2]) date[2] = 1;
+	var matchingDate = function matchingDate(date, mainDate) {
+	  mainDate.year(date[0] || mainDate.year());
+	  mainDate.month(date[1] || mainDate.month());
+	  mainDate.date(date[2] || mainDate.date());
+	  mainDate.hour(date[3] || mainDate.hour());
+	  mainDate.minute(date[4] || mainDate.minute());
+	  mainDate.second(date[5] || mainDate.second());
+	  mainDate.millisecond(date[6] || mainDate.millisecond());
+	  return mainDate.toArray();
+	};
 
-	  from = date.map(function (value, i) {
-	    return REGEX["isNumeric"].test(from[i]) ? from[i] : value;
-	  });
+	function isBetween(from, to, method) {
+	  from = typesToArray(this.c, from);
+	  from = matchingDate(from, this.clone());
 	  to = typesToArray(this.c, to);
-	  to = date.map(function (value, i) {
-	    return REGEX["isNumeric"].test(to[i]) ? to[i] : value;
-	  });
+	  to = matchingDate(to, this.clone());
 	  if (!(this.isValid.apply(this, _toConsumableArray(from)) && this.isValid.apply(this, _toConsumableArray(to)))) return false;
 	  return compare.call(this, from, method[0] === "[" ? ">=" : ">") && compare.call(this, to, method[1] === "]" ? "<=" : "<");
 	}
